571136f377eddcab63d2d9c2cb8bc1c8e57f3f55
Sebastian Korotkiewicz <skorotkiewicz@gmail.com>
2022-08-03 21:26:21 +0200

    add url to repo

--- src/main.tsx
+++ src/main.tsx
@@ -25,6 +25,9 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <GunContext gun={gun}>
       <Dashboard />
+      <footer>
+        <a href="https://github.com/skorotkiewicz/Licom-p2p">opensource</a>
+      </footer>
     </GunContext>
   </React.StrictMode>
 );
